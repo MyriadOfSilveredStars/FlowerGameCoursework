49a0c7b60e9f1dff48981b141ebc3e68051ce72f
Rho Bates <rhobates@outlook.com~>
2026-02-08 18:27:24 +0000

    Added the starting files, and the work already done, which includes creating the basic layout of the playable area and a cylinder to act as the player

---
+++ FlowerFarm/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{    
+    public float m_Speed = 12f;            
+    public float m_TurnSpeed = 180f;       
+     
+    private string m_MovementAxisName;     
+    private string m_TurnAxisName;         
+    private Rigidbody m_Rigidbody;         
+    private float m_MovementInputValue;    
+    private float m_TurnInputValue;        
+     
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable ()
+    {
+        m_Rigidbody.isKinematic = false;
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+    private void OnDisable ()
+    {
+        m_Rigidbody.isKinematic = true;
+    }
+
+    private void Start()
+    {
+        m_MovementAxisName = "Vertical";
+        m_TurnAxisName = "Horizontal";
+    }
+    
+
+    private void Update()
+    {
+        // Store the player's input and make sure the audio for the engine is playing.
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Adjust the rigidbodies position and orientation in FixedUpdate.
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        // Apply this movement to the rigidbody's position.
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
+
+    private void Turn()
+    {
+        // Determine the number of degrees to be turned based on the input, speed and time between frames.
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        // Make this into a rotation in the y axis.
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        // Apply this rotation to the rigidbody's rotation.
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+    }
+}
