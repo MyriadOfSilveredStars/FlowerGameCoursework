bc8f508c913e24681bd24fc5f62276f47ea7e7c6
Rho Bates <rhobates@outlook.com~>
2026-04-01 16:06:34 +0100

    Added a day counter, so you can see what day it is

--- FlowerFarm/FlowerFarmGame/Assets/Scripts/DayManager.cs
+++ FlowerFarm/FlowerFarmGame/Assets/Scripts/DayManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using TMPro;
 
 public class DayManager : MonoBehaviour, IInteractable 
 { //this will manage the days that the player has gone through
@@ -11,18 +12,24 @@ public class DayManager : MonoBehaviour, IInteractable
     public string Season;
 
     public GameObject[] plantSpots;
+
+    public TMP_Text dayCounterText;
+    public TMP_Text seasonCounterText;
     public static event Action<int> OnDayPassed;
 
     private void Start()
     {
-        daysGone = 0;
-        Season = "Spring";
+        daysGone = 1;
+        Season = "SPRING";
+        dayCounterText.text = "Day : " + daysGone.ToString();
+        seasonCounterText.text = Season;
     }
 
     public void nextDay()
     {
         daysGone += 1;
         OnDayPassed?.Invoke(1);
+        dayCounterText.text = "Day : " + daysGone.ToString();
     }
 
     public void changeSeason()
